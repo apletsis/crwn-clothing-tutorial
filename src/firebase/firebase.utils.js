614d3a048b5ef016bb4331195243af8641e63e2f
apletsis <alexanderpletsis@gmail.com>
2020-02-11 17:58:46 +0200

    firebase sign up and store users data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,9 +14,39 @@ const config = {
   measurementId: "G-23JKLQH7MB"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.error('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const firestore = firebase.firestore();
+//Some examlpes of using firestore
+//firestore.collection('users').doc('some_id').collection('cartItems').doc('some_id');
+//firestore.doc('/users/some_id/cartItems/some_id');
+//firestore.collection('/users/some_id/cartitems');
 export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
