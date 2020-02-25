58e59a50286adf424d87142aeebf53fb5add5551
apletsis <alexanderpletsis@gmail.com>
2020-02-25 13:13:24 +0200

    shop data array updated, cart items quantity added

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // set quantity value for the first time for the new item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+};
