7b7bc54d5346df8fd819007c14a32282b69503cf
apletsis <alexanderpletsis@gmail.com>
2020-06-24 13:05:38 +0300

    server.js path hotfix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 // serve front-end in the production build
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
